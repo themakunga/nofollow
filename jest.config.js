9ca9b792d17a74a0d8e4fafc9ad24ba900be3747
Nicolas Martinez V <nmartinezv@icloud.com>
2022-01-25 00:55:35 -0300

    chore: add default files

---
+++ jest.config.js
@@ -0,0 +1,35 @@
+export default {
+  verbose: true,
+  rootDir: '.',
+  testMatch: [
+    '**/__test__/**/*.test.js',
+  ],
+  collectConverage: true,
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/__fixtures__/',
+    '/(__)?test(s__)?/',
+    '/(__)?mock(s__)?',
+    '/__jest__/',
+    '.?.min.js',
+  ],
+  moduleDirectories: [
+    'node_modules',
+    'src',
+  ],
+  modulesFileExtensions: [
+    'js',
+    'jsx',
+    'json',
+  ],
+  moduleNameMapper: {
+    '^~/(.*)$': '<rootDir>/$1',
+    '^@@/(.*)$': '<rootDir>/src/$1',
+    '^@config': '<rootDir>/src/config/$1',
+    '^@controllers/(.*)$': '<rootDir>/src/controllers/$1',
+    '^@models/(.*)$': '<rootDir>/src/models/$1',
+    '^@repositories/(.*)$': '<rootDir>/src/repositories/$1',
+    '^@routes/(.*)$': '<rootDir>/src/routes/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+  },
+};
